9fec322d08c51be66eae3826460d5c5296d8f6e2
Arezlon <50326537+Arezlon@users.noreply.github.com>
2020-06-10 11:19:04 -0300

    Relaciones

--- universidad.sql
+++ universidad.sql
@@ -1,58 +1,121 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versión del servidor:         10.1.9-MariaDB-log - mariadb.org binary distribution
--- SO del servidor:              Win32
--- HeidiSQL Versión:             9.3.0.4984
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-06-2020 a las 16:18:26
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Volcando estructura de base de datos para universidad
-CREATE DATABASE IF NOT EXISTS `universidad` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `universidad`;
+--
+-- Base de datos: `universidad`
+--
 
+-- --------------------------------------------------------
 
--- Volcando estructura para tabla universidad.alumno
-CREATE TABLE IF NOT EXISTS `alumno` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Estructura de tabla para la tabla `alumno`
+--
+
+CREATE TABLE `alumno` (
+  `id` int(11) NOT NULL,
   `nombre` varchar(30) DEFAULT NULL,
   `fecNac` datetime DEFAULT NULL,
-  `activo` tinyint(4) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+  `activo` tinyint(4) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla universidad.alumno: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `alumno` DISABLE KEYS */;
-/*!40000 ALTER TABLE `alumno` ENABLE KEYS */;
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `cursada`
+--
 
--- Volcando estructura para tabla universidad.cursada
-CREATE TABLE IF NOT EXISTS `cursada` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `cursada` (
+  `id` int(11) NOT NULL,
   `idAlumno` int(11) NOT NULL,
   `idMateria` int(11) NOT NULL,
-  `nota` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `nota` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla universidad.cursada: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `cursada` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cursada` ENABLE KEYS */;
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `materia`
+--
 
--- Volcando estructura para tabla universidad.materia
-CREATE TABLE IF NOT EXISTS `materia` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `materia` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla universidad.materia: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `materia` DISABLE KEYS */;
-/*!40000 ALTER TABLE `materia` ENABLE KEYS */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `cursada`
+--
+ALTER TABLE `cursada`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idAlumno` (`idAlumno`),
+  ADD KEY `idMateria` (`idMateria`);
+
+--
+-- Indices de la tabla `materia`
+--
+ALTER TABLE `materia`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cursada`
+--
+ALTER TABLE `cursada`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `materia`
+--
+ALTER TABLE `materia`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cursada`
+--
+ALTER TABLE `cursada`
+  ADD CONSTRAINT `cursada_ibfk_1` FOREIGN KEY (`idAlumno`) REFERENCES `alumno` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `cursada_ibfk_2` FOREIGN KEY (`idMateria`) REFERENCES `materia` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
